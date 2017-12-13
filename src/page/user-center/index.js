95868e2648b659c007843dbc64d63bc531f6c8a5
2450756004@qq.com <2450756004@qq.com>
2017-12-13 10:17:43 +0800

    用户模块

---
+++ src/page/user-center/index.js
@@ -0,0 +1,41 @@
+/*
+* @Author: lenovo
+* @Date:   2017-12-13 09:35:14
+* @Last Modified by:   lenovo
+* @Last Modified time: 2017-12-13 10:01:02
+*/
+require('./index.css');
+require('page/common/nav/index.js');
+require('page/common/header/index.js');
+var navSide         = require('page/common/nav-side/index.js');
+var _mm             = require('util/mm.js');
+var _user           = require('service/user-service.js');
+var templateIndex   = require('./index.string');
+
+// page 逻辑部分
+var page = {
+    init: function(){
+        this.onLoad();
+    },
+    onLoad : function(){
+        // 初始化左侧菜单
+        navSide.init({
+            name: 'user-center'
+        });
+        // 加载用户信息
+        this.loadUserInfo();
+    },
+    // 加载用户信息
+    loadUserInfo : function(){
+        var userHtml = '';
+        _user.getUserInfo(function(res){
+            userHtml = _mm.renderHtml(templateIndex, res);
+            $('.panel-body').html(userHtml);
+        }, function(errMsg){
+            _mm.errorTips(errMsg);
+        });
+    }
+};
+$(function(){
+    page.init();
+});
