6abb9aa8af31c449f470b00cb851e43d4c85dd61
2450756004@qq.com <2450756004@qq.com>
2017-12-15 23:03:31 +0800

    商品模块

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,14 +2,27 @@
 * @Author: 2450756004@qq.com
 * @Date:   2017-12-10 21:45:03
 * @Last Modified by:   lenovo
-* @Last Modified time: 2017-12-12 20:42:19
+* @Last Modified time: 2017-12-15 17:29:51
 */
-'use strict';
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
 var navSide         = require('page/common/nav-side/index.js');
-var _mm = require('util/mm.js');
+var templateBanner  = require('./banner.string');
+var _mm             = require('util/mm.js');
 
-navSide.init({
-    name : 'order-list'
+$(function() {
+    // 渲染banner的html
+    var bannerHtml  = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider     = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
 });
---
+++ src/service/product-service.js
@@ -0,0 +1,31 @@
+/*
+* @Author: lenovo
+* @Date:   2017-12-15 21:12:38
+* @Last Modified by:   lenovo
+* @Last Modified time: 2017-12-15 21:13:15
+*/
+var _mm = require('util/mm.js');
+
+var _product = {
+    // 获取商品列表
+    getProductList : function(listParam, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/product/list.do'),
+            data    : listParam,
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取商品详细信息
+    getProductDetail : function(productId, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/product/detail.do'),
+            data    : {
+                productId : productId
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _product;
