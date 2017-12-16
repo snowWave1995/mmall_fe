38e1b33bc3b436a82b78b62cf961ee0d5ec775f6
2450756004@qq.com <2450756004@qq.com>
2017-12-16 23:17:31 +0800

    支付模块

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -2,7 +2,7 @@
 * @Author: lenovo
 * @Date:   2017-12-12 20:44:27
 * @Last Modified by:   lenovo
-* @Last Modified time: 2017-12-12 20:56:02
+* @Last Modified time: 2017-12-16 23:15:25
 */
 require('./index.css');
 require('page/common/nav-simple/index.js');
@@ -11,6 +11,11 @@ var _mm = require('util/mm.js');
 $(function(){
     var type        = _mm.getUrlParam('type') || 'default',
         $element    = $('.' + type + '-success');
+    if(type === 'payment'){
+        var orderNumber  = _mm.getUrlParam('orderNumber'),
+            $orderNumber = $element.find('.order-number');
+        $orderNumber.attr('href', $orderNumber.attr('href') + orderNumber);
+    }
     // 显示对应的提示元素
     $element.show();
 })
---
+++ src/service/payment-service.js
@@ -0,0 +1,33 @@
+/*
+* @Author: lenovo
+* @Date:   2017-12-16 23:08:40
+* @Last Modified by:   lenovo
+* @Last Modified time: 2017-12-16 23:09:01
+*/
+var _mm = require('util/mm.js');
+
+var _payment = {
+    // 获取支付信息
+    getPaymentInfo : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/pay.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 获取订单状态
+    getPaymentStatus : function(orderNumber, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/order/query_order_pay_status.do'),
+            data    : {
+                orderNo : orderNumber
+            },
+            success : resolve,
+            error   : reject
+        });
+    }
+}
+module.exports = _payment;
